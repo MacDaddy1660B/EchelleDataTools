01abeb720f6a073ce502db38fd712125680ecce2
Gordon A. MacDonald <gmacdonald@apo.nmsu.edu>
2025-11-24 11:25:45 -0700

    Updates to this script

--- example-scripts/comp_two_cal_sets.py
+++ example-scripts/comp_two_cal_sets.py
@@ -35,73 +35,57 @@ seqb.makeRedSuperFlat(biasSubtract=True)
 seqc.makeBlueSuperFlat(biasSubtract=True)
 seqc.makeRedSuperFlat(biasSubtract=True)
 
-biasDiffControl = SuperFrame(
-        data=seqc.superBiasFrame.data - seqa.superBiasFrame.data,
-        name='control',
+### Plot the bias frames
+EchellePlotTools.plotImageMulti(
+        seqa.biasFrames,
+        savefig=True,
+        fname='seqA_biasFrames.svg'
         )
-biasDiffExp = SuperFrame(
-        data=seqb.superBiasFrame.data - seqa.superBiasFrame.data,
-        name='experiment',
+EchellePlotTools.plotImageMulti(
+        seqb.biasFrames,
+        savefig=True,
+        fname='seqB_biasFrames.svg'
         )
-blueFlatDiffControl = SuperFrame(
-        data=seqc.superBlueFlatFrame.data - seqa.superBlueFlatFrame.data,
-        name='control',
-        )
-blueFlatDiffExp = SuperFrame(
-        data=seqb.superBlueFlatFrame.data - seqa.superBlueFlatFrame.data,
-        name='experiment',
-        )
-redFlatDiffControl = SuperFrame(
-        data=seqc.superRedFlatFrame.data - seqa.superRedFlatFrame.data,
-        name='control',
-        )
-redFlatDiffExp = SuperFrame(
-        data=seqb.superRedFlatFrame.data - seqa.superRedFlatFrame.data,
-        name='experiment',
+EchellePlotTools.plotImageMulti(
+        seqc.biasFrames,
+        savefig=True,
+        fname='seqC_biasFrames.svg'
         )
 
-### Plot the super bias differences
-#EchellePlotTools.plotImageAndHist(biasDiffControl,
-#        savefig=True,
-#        fname='superBiasControl.svg',
-#        )
-#EchellePlotTools.plotImageAndHist(biasDiffExp,
-#        savefig=True,
-#        fname='superBiasExp.svg',
-#        )
-#EchellePlotTools.plotImageAndHistMulti(
-#        [biasDiffControl,biasDiffExp,],
-#        savefig=True,
-#        fname='superBiasDiff.svg'
-#        )
-#
-### Plot the super flat differences
-#EchellePlotTools.plotImageAndHist(blueFlatDiffControl,
-#        savefig=True,
-#        fname='superBlueFlatDiffControl.svg',
-#        )
-#EchellePlotTools.plotImageAndHist(blueFlatDiffExp,
-#        savefig=True,
-#        fname='superBlueFlatDiffExp.svg',
-#        )
-#EchellePlotTools.plotImageAndHistMulti(
-#        [blueFlatDiffControl,blueFlatDiffExp,],
-#        savefig=True,
-#        fname='superBlueFlatDiff.svg'
-#        )
-#EchellePlotTools.plotImageAndHist(redFlatDiffControl,
-#        savefig=True,
-#        fname='superRedFlatDiffControl.svg',
-#        )
-#EchellePlotTools.plotImageAndHist(redFlatDiffExp,
-#        savefig=True,
-#        fname='superRedFlatDiffExp.svg',
-#        )
-#EchellePlotTools.plotImageAndHistMulti(
-#        [redFlatDiffControl,redFlatDiffExp,],
-#        savefig=True,
-#        fname='superRedFlatDiff.svg'
-#        )
+### Plot the bias histograms
+EchellePlotTools.plotHistMulti(
+        seqa.biasFrames,
+        savefig=True,
+        fname='seqA_biasFramesHist.svg'
+        )
+EchellePlotTools.plotHistMulti(
+        seqb.biasFrames,
+        savefig=True,
+        fname='seqB_biasFramesHist.svg'
+        )
+EchellePlotTools.plotHistMulti(
+        seqc.biasFrames,
+        savefig=True,
+        fname='seqC_biasFramesHist.svg'
+        )
+
+### Plot the super bias frames
+EchellePlotTools.plotImageAndHist(
+        seqa.superBiasFrame,
+        savefig=True,
+        fname='seqA_superBias.svg',
+        )
+EchellePlotTools.plotImageAndHist(
+        seqb.superBiasFrame,
+        savefig=True,
+        fname='seqB_superBias.svg',
+        )
+EchellePlotTools.plotImageAndHist(
+        seqc.superBiasFrame,
+        savefig=True,
+        fname='seqC_superBias.svg'
+        )
+
 
 rowc = seqa.biasFrames[0].data.shape[0]//2
 colc = seqa.biasFrames[0].data.shape[1]//2
