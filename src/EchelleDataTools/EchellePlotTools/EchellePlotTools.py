f7fbddbe0319daca7840c764f89a8423727d8f9d
Gordon A. MacDonald <gmacdonald@apo.nmsu.edu>
2025-11-24 10:34:01 -0700

    Add plotHistMulti and plotImageMulti functions.

--- src/EchelleDataTools/EchellePlotTools/EchellePlotTools.py
+++ src/EchelleDataTools/EchellePlotTools/EchellePlotTools.py
@@ -118,3 +118,88 @@ def plotImageAndHistMulti(frames : list, savefig=False, fname=None):
                 format='svg',
                 )
 
+
+def plotImageMulti(frames : list, savefig=False, fname=None):
+    """
+    """
+    if not isinstance(frames, list):
+        raise TypeError(f"Frames argument must be of type list.")
+
+    if not all( isinstance(f, BaseFrame) for f in frames):
+        raise TypeError(f"Objects in list frames must inherit EchelleDataTools.Frame.BaseFrame.")
+
+    numFrame = len(frames)
+
+    numRows = int(np.ceil( np.sqrt(numFrame) ))
+    fig, axes = plt.subplots( numRows, int(np.ceil(numFrame/numRows)), figsize=(8,10) )
+
+    if numFrame > 1:
+        axes = axes.flatten() ## Flatten axes grid so we can iterate through them.
+    for row, f in enumerate(frames):
+        stretch = HistEqStretch(f.data)
+        norm = ImageNormalize(f.data, stretch=stretch)
+
+        axes[row].imshow(
+                f.data,
+                origin='lower',
+                cmap='gray',
+                norm=norm,
+                interpolation='none',
+                )
+        axes[row].set_title(f.name.title() if isinstance(f.name, str) else "Frame")
+
+    for a in  axes[row+1:]:
+        a.set_axis_off()
+        
+    plt.tight_layout()
+    if not savefig:
+        plt.show()
+    else:
+        if not fname:
+            fname=frame.name
+        plt.savefig(
+                fname,
+                format='svg',
+                )
+
+def plotHistMulti(frames : list, savefig=False, fname=None):
+    """
+    """
+    if not isinstance(frames, list):
+        raise TypeError(f"Frames argument must be of type list.")
+
+    if not all( isinstance(f, BaseFrame) for f in frames):
+        raise TypeError(f"Objects in list frames must inherit EchelleDataTools.Frame.BaseFrame.")
+
+    numFrame = len(frames)
+
+    numRows = int(np.ceil( np.sqrt(numFrame) ))
+    fig, axes = plt.subplots( numRows, int(np.ceil(numFrame/numRows)), figsize=(8,10) )
+
+    if numFrame > 1:
+        axes = axes.flatten() ## Flatten axes grid so we can iterate through them.
+    for row, f in enumerate(frames):
+        
+        axes[row].hist(
+                f.data.ravel(),
+                bins=np.arange(0, (1 << 16)+1, 512), # 0 -- 65536, with 512 stepsize.
+                log=True,
+                )
+        axes[row].set_title(f.name.title() if isinstance(f.name, str) else "Frame")
+
+    for a in  axes[row+1:]:
+        a.set_axis_off()
+        
+    plt.tight_layout()
+    if not savefig:
+        plt.show()
+    else:
+        if not fname:
+            fname=frame.name
+        plt.savefig(
+                fname,
+                format='svg',
+                )
+
+
+
