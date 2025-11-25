f6ec6591e673de268ddb77e82a05c4dcb0150523
Gordon A. MacDonald <gmacdonald@apo.nmsu.edu>
2025-11-25 13:57:52 -0700

    Remove un-needed calulations and add some statistics to the output.

--- example-scripts/comp_three_cal_sets.py
+++ example-scripts/comp_three_cal_sets.py
@@ -18,22 +18,20 @@ seqb = EchelleDataSequence(DATAB)
 seqc = EchelleDataSequence(DATAC)
 
 ## Load the images. Cal sets dont have dark or object frames
-seqa.loadFrames(loadObjectFrames=False, loadDarkFrames=False)
-seqb.loadFrames(loadObjectFrames=False, loadDarkFrames=False)
-seqc.loadFrames(loadObjectFrames=False, loadDarkFrames=False)
+seqa.loadFrames(loadObjectFrames=False, loadDarkFrames=False,
+        loadBlueFlatFrames=False, loadRedFlatFrames=False, loadWaveCalFrames=False
+        )
+seqb.loadFrames(loadObjectFrames=False, loadDarkFrames=False,
+        loadBlueFlatFrames=False, loadRedFlatFrames=False, loadWaveCalFrames=False
+        )
+seqc.loadFrames(loadObjectFrames=False, loadDarkFrames=False,
+        loadBlueFlatFrames=False, loadRedFlatFrames=False, loadWaveCalFrames=False
+        )
 
 ## Make super bias framess
 seqa.makeSuperBias()
 seqb.makeSuperBias()
 seqc.makeSuperBias()
-
-## Make super flat frames
-seqa.makeBlueSuperFlat(biasSubtract=True)
-seqa.makeRedSuperFlat(biasSubtract=True)
-seqb.makeBlueSuperFlat(biasSubtract=True)
-seqb.makeRedSuperFlat(biasSubtract=True)
-seqc.makeBlueSuperFlat(biasSubtract=True)
-seqc.makeRedSuperFlat(biasSubtract=True)
 
 ### Plot the bias frames
 EchellePlotTools.plotImageMulti(
@@ -87,14 +85,14 @@ EchellePlotTools.plotImageAndHist(
         )
 
 
-rowc = seqa.biasFrames[0].data.shape[0]//2
-colc = seqa.biasFrames[0].data.shape[1]//2
-box = 32
-boxGrid = np.meshgrid(
-        np.arange(rowc-box//2, rowc+box//2),
-        np.arange(colc-box//2, colc+box//2),
-        indexing='ij'
-        )
+#rowc = seqa.biasFrames[0].data.shape[0]//2
+#colc = seqa.biasFrames[0].data.shape[1]//2
+#box = 32
+#boxGrid = np.meshgrid(
+#        np.arange(rowc-box//2, rowc+box//2),
+#        np.arange(colc-box//2, colc+box//2),
+#        indexing='ij'
+#        )
 
 print("Single-sample t-tests:")
 tTestCont = EchelleStatsTools.EchelleTtestSingle(
@@ -111,14 +109,17 @@ print(f"Experiment: {tTestExp}")
 
 print("Independent t-tests:")
 tTestIndCont = EchelleStatsTools.EchelleTtestIndep(
-        np.array( [f.data[boxGrid] for f in seqc.biasFrames]),
-        np.array( [f.data[boxGrid] for f in seqa.biasFrames]),
+        np.array( [f.data for f in seqc.biasFrames]),
+        np.array( [f.data for f in seqa.biasFrames]),
         )
 print(f"Control: {tTestIndCont}")
 tTestIndExp = EchelleStatsTools.EchelleTtestIndep(
-        np.array( [f.data[boxGrid] for f in seqb.biasFrames]),
-        np.array( [f.data[boxGrid] for f in seqa.biasFrames]),
+        np.array( [f.data for f in seqb.biasFrames]),
+        np.array( [f.data for f in seqa.biasFrames]),
         )
 print(f"Experiment: {tTestIndExp}")
 
-print(f"{stats.levene(seqc.biasFrames[0].data[boxGrid].ravel(),  seqa.biasFrames[0].data[boxGrid].ravel())}")
+print("Super-bias Statistics:")
+print(f"Seq A: {seqa.superBiasFrame.data.mean()=} {seqa.superBiasFrame.data.std()=}")
+print(f"Seq B: {seqb.superBiasFrame.data.mean()=} {seqb.superBiasFrame.data.std()=}")
+print(f"Seq C: {seqc.superBiasFrame.data.mean()=} {seqc.superBiasFrame.data.std()=}")
